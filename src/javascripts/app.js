c91c374d9908fb781efdd8bcf8665adb5e9a13d2
Jared T <Jared@jaredmakes.com>
2019-09-22 10:24:27 -0400

    works!

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,14 +1,51 @@
 import { $, $$ } from './lib/bling';
 
+const RecordRTC = require('recordrtc');
+
+let recorder;
+
 const video = document.querySelector('#videoPlayer');
 
-if (navigator.mediaDevices.getUserMedia) {
+function captureCamera(callback) {
   navigator.mediaDevices
-    .getUserMedia({ video: true })
-    .then(stream => {
-      video.srcObject = stream;
+    .getUserMedia({ audio: true, video: true })
+    .then(camera => {
+      callback(camera);
     })
-    .catch(e => {
-      console.log(`Something went wrong! ${e}`);
+    .catch(error => {
+      console.error(`ERROR ${error}`);
     });
 }
+
+function stopRecordingCallback() {
+  video.srcObject = null;
+  video.src = video.srcObject;
+  video.muted = false;
+  video.volume = 1;
+  video.src = URL.createObjectURL(recorder.getBlob());
+
+  recorder.camera.stop();
+  recorder.destroy();
+  recorder = null;
+}
+
+document.getElementById('start').onclick = function () {
+  this.disabled = true;
+  captureCamera(camera => {
+    video.muted = true;
+    video.volume = 0;
+    video.srcObject = camera;
+    recorder = RecordRTC(camera, {
+      type: 'video',
+    });
+    recorder.startRecording();
+    // release camera on stopRecording
+    recorder.camera = camera;
+    document.getElementById('btn-stop-recording').disabled = false;
+  });
+};
+
+document.getElementById('stop').onclick = function () {
+  this.disabled = true;
+  recorder.stopRecording(stopRecordingCallback);
+};
